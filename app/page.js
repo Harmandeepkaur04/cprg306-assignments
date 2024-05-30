0b90e36869627541aecc7ca71cabbfeec395e5c7
Harmandeep_Kaur <Harmandeep.Kaur04@edu.sait.ca>
2024-05-30 16:34:30 -0600

    Week-03
    Made a shopping list and styled using CSS.

--- app/page.js
+++ app/page.js
@@ -5,7 +5,8 @@ export default function Home() {
   return (
     <main> 
       <h1>cprg306:Web Development 2-Assignments</h1>
-      <Link className="underline text-cyan-600 hover:text-cyan-300" href="./week-2">week-2</Link>
+      <Link className="underline text-cyan-600 hover:text-cyan-300" href="./week-3">week-3</Link>
+
     </main>
   );
 }
